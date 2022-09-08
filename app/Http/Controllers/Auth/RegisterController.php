0ab403fb49ced582c5e47d619f5e2d02b9cb2a55
yukimasaki <woodenhoe@gmail.com>
2022-09-08 08:12:41 +0000

    registeredメソッドを追加

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -8,6 +8,7 @@ use App\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Request;
 
 class RegisterController extends Controller
 {
@@ -70,4 +71,11 @@ class RegisterController extends Controller
             'password' => Hash::make($data['password']),
         ]);
     }
+
+    //追加
+    protected function registered(Request $request, $user)
+    {
+        return $user;
+    }
+
 }
