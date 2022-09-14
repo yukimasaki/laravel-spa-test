e9787ab3a760bffbec15e331f2b1adb95184d505
yukimasaki <woodenhoe@gmail.com>
2022-09-14 01:11:34 +0000

    ログアウトAPI実装

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -44,4 +44,12 @@ class LoginController extends Controller
         return $user;
     }
 
+    protected function loggedOut(Request $request)
+    {
+        // セッションを再生成する
+        $request->session()->regenerate();
+    
+        return response()->json();
+    }
+    
 }
