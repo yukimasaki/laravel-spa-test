26c71298b56ecfeb77e30dec00030b2fe2a27dc1
yukimasaki <woodenhoe@gmail.com>
2022-09-14 06:54:01 +0000

    Vuexを読み込むようapp.jsを修正

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,12 +2,14 @@ import './bootstrap'
 import Vue from 'vue';
 import Vuetify from './plugins/vuetify'
 import router from './router'
+import store from './store'
 import App from './App.vue'
 
 const app = new Vue({
     el: '#app',
     vuetify: Vuetify,
     router,
+    store,
     components: {
         App,
     },
