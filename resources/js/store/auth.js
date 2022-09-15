129862a911d2fdcc2302be16294084a55d32590e
yukimasaki <woodenhoe@gmail.com>
2022-09-15 09:27:08 +0000

    authモジュール修正

--- resources/js/store/auth.js
+++ resources/js/store/auth.js
@@ -1,5 +1,8 @@
+import { OK } from '../util'
+
 const state = {
-  user: null
+  user: null,
+  apiStatus: null
 }
 
 const getters = {
@@ -10,6 +13,9 @@ const getters = {
 const mutations = {
   setUser (state, user) {
     state.user = user
+  },
+  setApiStatus (state, status) {
+    state.apiStatus = status
   }
 }
 
@@ -22,8 +28,18 @@ const actions = {
 
   //ログイン
   async login (context, data) {
+    context.commit('setApiStatus', null)
     const response = await axios.post('/api/login', data)
-    context.commit('setUser', response.data)
+      .catch(err => err.response || err)
+
+    if (response.status === OK) {
+      context.commit('setApiStatus', true)
+      context.commit('setUser', response.data)
+      return false
+    }
+
+    context.commit('setApiStatus', false)
+    context.commit('error/setCode', response.status, { root: true })
   },
 
   //ログアウト
