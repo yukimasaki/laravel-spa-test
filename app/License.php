b75d2570d3df1c34438fdf90ae20f2329b2859d6
yukimasaki <woodenhoe@gmail.com>
2022-09-16 04:35:23 +0000

    Licenseモデルに書き換え許可設定を追加

--- app/License.php
+++ app/License.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class License extends Model
 {
-    //
+    //書き換えを許可するカラム
+    protected $fillable = [
+      'product_key',
+      'product_name',
+      'expire_date',
+      'is_notify',
+      'assigned_to',
+      'customer_id'
+    ]
 }
