6a21419b71ad3994d0b4a3efd915ddbebe45ed4a
yukimasaki <woodenhoe@gmail.com>
2022-09-16 04:38:30 +0000

    Licenseコントローラー、APIを作成

--- routes/api.php
+++ routes/api.php
@@ -3,14 +3,18 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+// 認証関係
+//////////////////////////////////////////////////////////
 // 会員登録
 Route::post('/register', 'Auth\RegisterController@register')->name('register');
-
 // ログイン
 Route::post('/login', 'Auth\LoginController@login')->name('login');
-
 // ログアウト
 Route::post('/logout', 'Auth\LoginController@logout')->name('logout');
-
 // ログインユーザー
 Route::get('/user', fn() => Auth::user())->name('user');
+
+// モデル関係
+//////////////////////////////////////////////////////////
+// License
+Route::resource('/licenses', App\Http\Controllers\LicenseController::class);
