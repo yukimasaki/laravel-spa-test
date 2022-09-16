98f004cad04fd22b32342f572593904ded6a48a7
yukimasaki <woodenhoe@gmail.com>
2022-09-16 04:30:10 +0000

    Licenseモデル・マイグレーションファイル作成

---
+++ database/migrations/2022_09_16_132233_create_licenses_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLicensesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('licenses', function (Blueprint $table) {
+            $table->id();
+            $table->string('product_key');
+            $table->string('product_name');
+            $table->date('expire_date');
+            $table->boolean('is_notify');
+            $table->string('assigned_to');
+            $table->unsignedBigInteger('customer_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('licenses');
+    }
+}
